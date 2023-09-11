a7d36ce6c3b570ed110f57c7e443f2172fb1aa37
raunakpurohit <raunak.wscubetech@gmail.com>
2023-09-11 18:52:43 +0530

    Initial Commit

---
+++ app/src/main/java/com/alphabet/webbrowser137/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.alphabet.webbrowser137
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebViewClient
+import com.alphabet.webbrowser137.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.imgSearch.setOnClickListener {
+            val mQuery = binding.edtSearch.text.toString()
+
+            if(mQuery!=""){
+                binding.webView.loadUrl("https://www.google.com/search?q=$mQuery")
+                binding.webView.webViewClient = WebViewClient()
+            }
+
+            //yahoo
+        }
+
+    }
+}
